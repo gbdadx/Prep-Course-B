e7dff85c2c937b2b6f3a11153547050330fb1e7a
gabriela diaz <gbdadx@gmail.com>
2022-10-17 11:11:29 -0300

    ejercicios unidad 3

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,40 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    let acum=0;
+    for(let i=1; i<=10;i++){
+      acum+=i;
+    }
+    console.log(acum);
+    return acum;
 }
-
+////////////////////////
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  return array.filter((elemento)=>elemento%2===0);
 }
-
+/////////////////////////////
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map((e)=>e**2);
 }
-
+//////////////////
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((i,e)=>i+e);
 }
+////////////////////
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return String(num).length;
 }
   
   
