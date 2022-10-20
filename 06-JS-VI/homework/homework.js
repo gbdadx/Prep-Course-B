46b627e4dceaf8e1a4b1280e29ee16aff295e8b5
gabriela diaz <gbdadx@gmail.com>
2022-10-20 12:54:03 -0300

    ejercicio js7 callbacks

--- 06-JS-VI/homework/homework.js
+++ 06-JS-VI/homework/homework.js
@@ -4,30 +4,46 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  /* con split y join
+  var palabra= nombre;
+  var uno= palabra.split('');
+  uno[0]=uno[0].toUpperCase()
+  palabra=uno.join('');
+  return palabra;*/
+
+  return nombre[0].toUpperCase() + nombre.slice(1);
+
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
+
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-  //Tu código:
+  /*Tu código:
+   numeros.cb(numeros);*/
+  var suma2 = numeros.reduce( (acc, ele)=> acc + ele);
+  cb(suma2);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  return array.forEach(cb)
 }
 
 function map(array, cb) {
@@ -35,13 +51,20 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const array2 = array.map(cb);
+  return array2;
 }
-
+///////////////////////////////////////
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const filtrados = array.filter(word => word[0]==='a');
+
+  return filtrados;
+
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
